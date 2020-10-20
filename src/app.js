69273c2a3b98a33b95acddbc16af0e3e9eafadec
unknown <arrajput33@gmail.com>
2020-10-20 12:16:58 +0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,97 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode= require('./utils/geocode')
+const forecast= require('./utils/forecast')
+
+const app = express()
+
+// define paths for express configs
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath= path.join(__dirname,'../templates/views')
+const partialspath = path.join(__dirname,'../templates/partials')
+
+// setup handle bar engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialspath)
+
+// setup the static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Rehman Niaz'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Rehman Niaz'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name:'Rehman Niaz'
+    })
+})
+app.get('/weather', (req, res) => {
+    if(!req.query.address)
+    {
+        return res.send({
+            error: 'please provide address'
+        })
+    }
+
+    geocode(req.query.address, (error,{latitude,longitude,location}={}) =>{
+        if (error){
+            return res.send({error})
+        }
+        forecast(latitude,longitude, (error , forecastData) =>{
+            if (error){
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address:req.query.address
+            })
+
+        })
+
+    })
+
+    
+})
+app.get('/products', (req, res) => {
+    if(!req.query.products){
+        return res.send({
+            error:'You must provide a search term!'
+        })
+
+    }
+    console.log(req.query.products)
+    res.send({
+        products:[]
+    })
+})
+app.get('/help/*',(req,res) =>{
+    res.render('404-page',{
+        title: '404',
+        name: 'Rehman Niaz',
+        unableFindpage:'Help article is not found!!!'
+    })
+})
+app.get('*',(req,res) =>{
+    res.render('404-page',{
+        title: '404',
+        name: 'Rehman Niaz',
+        unableFindpage:'Error 404 page not found!!!'
+    })
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
